8ecdc54b915eaed71f91f8d89344e40d96b53b0e
Valentin Portillo <valeporti@hotmail.com>
2018-07-07 10:49:36 -0500

    still configuring, have errors in heroku

--- includes/config.php
+++ includes/config.php
@@ -24,7 +24,7 @@
     session_start();
 
     // require authentication for all pages except /login.php, /logout.php, and /register.php
-    if (!in_array($_SERVER["PHP_SELF"], ["/login.php", "/logout.php", "/register.php"]))
+    if (!in_array($_SERVER["PHP_SELF"], ["/public/login.php", "/public/logout.php", "/public/register.php"]))
     {
         if (empty($_SESSION["id"]))
         {
